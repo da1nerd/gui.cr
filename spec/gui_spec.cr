52532d4866814559ead2a2136a628577b827b006
Joel <joel@neutrinographics.com>
2020-06-12 23:04:43 +0700

    added ci flag to travis tests to skip certain tests on ci

--- spec/gui_spec.cr
+++ spec/gui_spec.cr
@@ -1,7 +1,9 @@
 require "./spec_helper"
 
 describe GUI do
-  it "works" do
-    Prism::Context.run("Hello World", GUI::Engine.new)
-  end
+  {% if !flag?(:ci) %}
+    it "works" do
+      Prism::Context.run("Hello World", GUI::Engine.new)
+    end
+  {% end %}
 end
--- src/constraint_factory.cr
+++ src/constraint_factory.cr
@@ -2,13 +2,13 @@ require "./constraint.cr"
 require "./constraints.cr"
 
 module GUI::ConstraintFactory
-    extend self
+  extend self
 
-    def get_fill : GUI::Constraints
-        x = GUI::Constraint.new
-        y = GUI::Constraint.new
-        width = GUI::Constraint.new
-        height = GUI::Constraint.new
-        GUI::Constraints.new(x, y, width, height)
-    end
+  def get_fill : GUI::Constraints
+    x = GUI::Constraint.new
+    y = GUI::Constraint.new
+    width = GUI::Constraint.new
+    height = GUI::Constraint.new
+    GUI::Constraints.new(x, y, width, height)
+  end
 end
